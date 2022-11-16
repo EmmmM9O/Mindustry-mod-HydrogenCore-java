aca0e7401caffcb85a53e41d2ba2ebbefd1844f7
EmmmM9O <111637956+EmmmM9O@users.noreply.github.com>
2022-11-16 12:44:29 +0800

    Update IncludeBlock.java

--- src/hc/blocks/IncludeBlock.java
+++ src/hc/blocks/IncludeBlock.java
@@ -18,38 +18,7 @@ public class IncludeBlock extends StructureBlock{
         public Liquid[] OutLiquid;
         public StructB[] Apis;
         public int cnt;
-        @Override
-        public void BuildInit(){
-            cnt=0;
-            Apis=new StructB[20];
-            for(int i=0;i<NeedBlock.length;i++){
-                StructB a=NeedBlock[i];
-
-                if(a.block=="APIBlock"){
-
-                    Apis[cnt]= a;
-                    cnt++;
-                    var tile=world.tile((int)x/8+a.x,(int)y/8+a.y);
-                    BuildMap<APIBlock.APIBuild> block=hcore.list.<APIBlock.APIBuild>get(tile);
-                    APIBlock.APIBuild bu=block.build;
-                    bu.IsStruct=true;
-
-
-
-                }
-            }
-        }
-        @Override
-        public void MakeRun(){
-            for(int i=0;i<cnt;i++){
-                var a=Apis[i];
-
-                var tile=world.tile((int)x/8+a.x,(int)y/8+a.y);
-                BuildMap<APIBlock.APIBuild> block=hcore.list.get(tile);
-                APIBlock.APIBuild bu=block.build;
-                bu.IsStruct=true;
-            }
-        }
+        
 
     }
     
