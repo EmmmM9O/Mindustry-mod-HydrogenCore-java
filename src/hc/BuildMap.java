73663e3bda3f7c9105c44d18b1792f7d96fa3146
EmmmM9O <111637956+EmmmM9O@users.noreply.github.com>
2022-11-19 20:47:03 +0800

    Add files via upload

--- src/hc/BuildMap.java
+++ src/hc/BuildMap.java
@@ -3,9 +3,9 @@ package hc;
 import mindustry.gen.Building;
 import mindustry.world.Tile;
 public class BuildMap <T extends Building>{
-    public T build;
+    public Reference<T> build;
     public Tile tile;
     public BuildMap (T b,Tile t){
-        build=b;tile=t;
+        build=new SoftReference<T>(b);tile=t;
     }
 }
